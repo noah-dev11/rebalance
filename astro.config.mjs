370c74b8fda7159fe9936c366701d9c7f7258ac2
Andriel Noa <isnoah2020@gmail.com>
2025-02-03 13:47:06 -0500

    Netlify Support

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,10 +3,12 @@ import tailwind from "@astrojs/tailwind";
 import mdx from "@astrojs/mdx";
 import sitemap from "@astrojs/sitemap";
 import icon from "astro-icon";
-
+import netlify from '@astrojs/netlify';
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://astroship.web3templates.com",
   integrations: [tailwind(), mdx(), sitemap(), icon()],
+  output: 'server',
+  adapter: netlify(),
 });
